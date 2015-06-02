8569da4a062bba7a247af9602e2ad1c85fa0d7e5
javierivanov <j.fuentes.m@icloud.com>
2015-06-02 03:12:12 -0300

    improved snake

--- Snake/src/snake/Motor.java
+++ Snake/src/snake/Motor.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Arrays;
 
 
 /**
@@ -13,21 +14,26 @@ import java.awt.event.KeyListener;
 public class Motor implements KeyListener{
 
     public Point culebrita;
-    //public Point[] viborita;
-    //public int largo;
+    public Point[] viborita;
+    public int largo;
     public Point comida;
     public Dimension size;
     public String last;
     private int speed = 500;
     public int psize;
+    public Point cola;
+    public boolean alive = true;
+    public boolean pause = false;
     public Motor(Dimension size, int psize)
     {
         this.size = size;
         this.psize = psize;
         last="der";
-        //vivorita = new Point[20];
-        //largo = 1;
-        //vivorita = new Point(0,0);
+        viborita = new Point[150];
+        largo = 2;
+        viborita[1] = new Point(0, 0);
+        viborita[0] = new Point(psize,0);
+        cola = viborita[1];
         culebrita = new Point(0, 0);
         comida = new Point(5*psize, 5*psize);
         Thread t1;
@@ -36,22 +42,24 @@ public class Motor implements KeyListener{
             @Override
             public void run() {
                 int counter=0;
-                while(true)
+                while(alive)
                 {
-                    if (counter++ == 4)
-                    {
-                        counter=0;
-                        speed-=10;
-                    }
-                    if (speed<70) speed=70;
+                    
                     try{
                         Thread.sleep(speed);
                     } catch(Exception e){}
-                    if ("der".equals(last)) moverDer();
-                    if ("izq".equals(last)) moverIzq();
-                    if ("aba".equals(last)) moverAba();
-                    if ("arr".equals(last)) moverArr();
-                    hayComida();
+                    if (!pause)
+                    {
+                        if (counter++ == 4)
+                        {
+                            counter=0;
+                            speed-=50;
+                        }
+                        if (speed<70) speed=70;
+                        mover();
+                        estaViva();
+                        hayComida();
+                    }
                 }
             }
         });
@@ -60,46 +68,65 @@ public class Motor implements KeyListener{
 
     public boolean estaViva()
     {
-        return true;
+        return false;
     }
 
+    public void mover()
+    {
+        cola = new Point(viborita[largo-1]);
+        for (int i=0; i < largo-1; i++)
+        {
+            viborita[largo-1-i] = viborita[largo-2-i];
+        }
+        
+        if ("der".equals(last))
+        {
+            viborita[0] = new Point(viborita[0].x+psize, viborita[0].y);
+        }
+        if ("izq".equals(last))
+        {
+            viborita[0] = new Point(viborita[0].x-psize, viborita[0].y);
+        }
+        if ("aba".equals(last))
+        {
+            viborita[0] = new Point(viborita[0].x, viborita[0].y+psize);
+        }
+        if ("arr".equals(last))
+        {
+            viborita[0] = new Point(viborita[0].x, viborita[0].y-psize);
+        }
+        
+    }
+    
     public void nuevoElemento()
     {
-        
+        viborita[largo++] = cola;
     }
     
     public boolean hayComida()
     {
-        if (culebrita.x == comida.x && culebrita.y == comida.y)
+        if (viborita[0].x == comida.x && viborita[0].y == comida.y)
         {
-            comida.y = (int)(Math.random()*size.height/psize)*psize;
-            comida.x = (int)(Math.random()*size.width/psize)*psize;
+            nuevoElemento();
+            while (true)
+            {
+                
+                comida.y = (int)(Math.random()*(size.height/psize-1))*psize;
+                comida.x = (int)(Math.random()*(size.width/psize-1))*psize;
+                boolean next=false;
+                for (int i=0; i < largo; i++)
+                {
+                    if (comida.equals(viborita[i]))
+                    {
+                        next = true;
+                    }
+                }
+                if (!next) break;
+            }
             return true;
         }
         return false;
     }
-
-    public void moverIzq()
-    {
-        culebrita.x-=psize;
-    }
-
-    public void moverDer()
-    {
-
-        culebrita.x+=psize;
-    }
-
-    public void moverArr()
-    {
-        culebrita.y-=psize;
-    }
-
-    public void moverAba()
-    {
-        culebrita.y+=psize;
-    }
-
 
 
     @Override
@@ -113,6 +140,8 @@ public class Motor implements KeyListener{
         if (e.getKeyCode() == KeyEvent.VK_DOWN) last = "aba";
         if (e.getKeyCode() == KeyEvent.VK_LEFT) last = "izq";
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) last = "der";
+        if (e.getKeyCode() == KeyEvent.VK_P) pause = !pause;
+        if (e.getKeyCode() == KeyEvent.VK_Q) System.exit(0);
     }
 
     @Override
--- Snake/src/snake/Panel.java
+++ Snake/src/snake/Panel.java
@@ -4,11 +4,13 @@ package snake;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
 public class Panel extends JPanel{
     private Motor m;
+    public DeadAnimation dA;
     public Panel(Motor m, Dimension size)
     {
         this.m = m;
@@ -20,7 +22,7 @@ public class Panel extends JPanel{
                 {
                     repaint();
                     try{
-                        Thread.sleep(10);
+                        Thread.sleep(20);
                     } catch(Exception ex)
                     {}
                 }
@@ -55,16 +57,18 @@ public class Panel extends JPanel{
         g.fillRect(m.comida.x, m.comida.y, m.psize, m.psize);
     }
 
-    /*
+    
     public void pintarViborita(Graphics g)
     {
-        g.setColor(Color.yellow);
+        
         for (int i=0; i < m.largo; i++)
         {
+            
+            g.setColor(new Color(255-i/2,255-i*3, 255-i*2));
             g.fillRect(m.viborita[i].x, m.viborita[i].y, m.psize, m.psize);
         }
     }
-    */
+
 
     @Override
     public void paintComponent(Graphics g)
@@ -72,7 +76,56 @@ public class Panel extends JPanel{
         super.paintComponent(g);
         pintarFondo(g);
         pintarComida(g);
-        pintarPunto(g);
-        //pintarViborita(g);
+        //pintarPunto(g);
+        pintarViborita(g);
+        if (!m.alive)
+        {
+            if (dA == null)
+            {
+                dA = new DeadAnimation(getSize());
+            }
+            dA.pintarMuerte(g);
+        }
+        if (m.pause)
+        {
+            g.setColor(new Color(100, 100, 100, 200));
+            g.fillRect(0, 0, m.size.width, m.size.height);
+        }
+    }
+}
+
+
+class DeadAnimation
+{
+    public int dead=0;
+    public Dimension dim;
+    public DeadAnimation(Dimension size)
+    {
+        dim = size;
+        Thread t = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (dead < size.height)
+                {
+                    try{
+                        Thread.sleep((int)(Math.random()*5));
+                    }catch(Exception e){}
+                    dead++;
+                }
+                JOptionPane.showMessageDialog(null, "Perdiste!!!!");
+                System.exit(0);
+            }
+        });
+        t.start();
+    }
+    
+    public void pintarMuerte(Graphics g)
+    {
+        g.setColor(Color.red);
+        for (int i=0; i < dead; i++)
+        {
+            g.fillRect(0, i, dim.width, dead);
+        }
     }
 }
